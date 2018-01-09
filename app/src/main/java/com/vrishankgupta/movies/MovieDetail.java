537307377dceaea27fea9be8f4b5b549882716d0
vrishank <vrishank98@gmail.com>
2018-01-09 19:26:02 +0530

    Trailers added

--- app/src/main/java/com/vrishankgupta/movies/MovieDetail.java
+++ app/src/main/java/com/vrishankgupta/movies/MovieDetail.java
@@ -1,7 +1,12 @@
 package com.vrishankgupta.movies;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -11,11 +16,26 @@ import com.squareup.picasso.Picasso;
 import com.vrishankgupta.movies.Movies.TopRatedMovie;
 import com.vrishankgupta.movies.Movies.UpcomingMovies;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+
 public class MovieDetail extends AppCompatActivity {
 
     ImageView detailImage;
     TextView detailTitle,date,rating,popularity,overview;
     Button youButMovie;
+    String key;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +61,8 @@ public class MovieDetail extends AppCompatActivity {
                 rating.setText(rating.getText()+String.valueOf(topRatedMovie.getVote_average()));
                 popularity.setText(popularity.getText()+"Not Available");
                 overview.setText(topRatedMovie.getOverview());
+                new myTask().execute("http://api.themoviedb.org/3/movie/"+topRatedMovie.getId() +"/videos?api_key=091aa3d78da969a59546613254d71896");
+
             }
             else
                 Toast.makeText(this, "No TopRatedMovie or Popular", Toast.LENGTH_SHORT).show();
@@ -57,6 +79,7 @@ public class MovieDetail extends AppCompatActivity {
                 rating.setText(rating.getText()+String.valueOf(upcomingMovies.getVote_average()));
                 popularity.setText(popularity.getText()+upcomingMovies.getPopularity());
                 overview.setText(upcomingMovies.getOverview());
+                new myTask().execute("http://api.themoviedb.org/3/movie/"+upcomingMovies.getId() +"/videos?api_key=091aa3d78da969a59546613254d71896");
             }
             else
                 Toast.makeText(this, "No Upcoming", Toast.LENGTH_SHORT).show();
@@ -65,5 +88,61 @@ public class MovieDetail extends AppCompatActivity {
         else
             Toast.makeText(this, "Some Error", Toast.LENGTH_SHORT).show();
 
+        youButMovie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("TYD", "onClick: ");
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + key));
+                startActivity(i);
+            }
+        });
+
+    }
+
+
+    class myTask extends AsyncTask<String,Void,String>
+    {
+        @Override
+        protected String doInBackground(String... params) {
+            URL url = null;
+            try {
+                url = new URL(params[0]);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            try {
+                HttpURLConnection urlConnection = null;
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String s = bufferedReader.readLine();
+                bufferedReader.close();
+
+                return s;
+
+            } catch (IOException e) {
+                Log.e("Error: ", e.getMessage(), e);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            JSONObject jsonObject;
+
+            try {
+                jsonObject = new JSONObject(s);
+                JSONArray jsonArray = jsonObject.getJSONArray("results");
+
+                JSONObject jsonObject1 = jsonArray.getJSONObject(0);
+                key = jsonObject1.getString("key");
+                Log.d("Youtube", key);
+//                INTENT for Youtube
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- app/src/main/java/com/vrishankgupta/movies/TvDetail.java
+++ app/src/main/java/com/vrishankgupta/movies/TvDetail.java
@@ -1,8 +1,13 @@
 package com.vrishankgupta.movies;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -10,16 +15,30 @@ import android.widget.Toast;
 import com.squareup.picasso.Picasso;
 import com.vrishankgupta.movies.TvShow.Tv;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class TvDetail extends AppCompatActivity {
 
     ImageView detailImage1;
     TextView detailTitle1,date1,rating1,overview1;
+    String key;
+    Button youButTv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tv_detail);
         detailImage1 = findViewById(R.id.detailImage1);
-
+        youButTv = findViewById(R.id.youButTv);
         detailTitle1 = findViewById(R.id.detailTitle1);
         date1 = findViewById(R.id.date1);
         rating1 = findViewById(R.id.rating1);
@@ -34,8 +53,65 @@ public class TvDetail extends AppCompatActivity {
             date1.setText(show.getFirst_air_date());
             rating1.setText(rating1.getText()+String.valueOf(show.getVote_average()));
             overview1.setText(show.getOverview());
+            new myTask().execute("http://api.themoviedb.org/3/tv/"+show.getId() +"/videos?api_key=091aa3d78da969a59546613254d71896");
+
         }
         else
             Toast.makeText(this, "Some Error", Toast.LENGTH_SHORT).show();
+
+        youButTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("TYD", "onClick: ");
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + key));
+                startActivity(i);
+            }
+        });
+    }
+
+    class myTask extends AsyncTask<String,Void,String>
+    {
+        @Override
+        protected String doInBackground(String... params) {
+            URL url = null;
+            try {
+                url = new URL(params[0]);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            try {
+                HttpURLConnection urlConnection = null;
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String s = bufferedReader.readLine();
+                bufferedReader.close();
+
+                return s;
+
+            } catch (IOException e) {
+                Log.e("Error: ", e.getMessage(), e);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            JSONObject jsonObject;
+
+            try {
+                jsonObject = new JSONObject(s);
+                JSONArray jsonArray = jsonObject.getJSONArray("results");
+
+                JSONObject jsonObject1 = jsonArray.getJSONObject(0);
+                key = jsonObject1.getString("key");
+                Log.d("Youtube", key);
+//                INTENT for Youtube
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
